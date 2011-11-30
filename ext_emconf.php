315a669873fe3b20f65963f89314e0982188f0ca
Christian Zenker <christian.zenker@599media.de>
2011-11-30 11:41:50 +0100

    initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,35 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Generate PDFs using wkhtmltopdf',
+	'description' => 'A wrapper to let TYPO3 generate PDF files from html pages. Uses wkhtmltopdf,
+	a binary that is using the print functionality of the webkit render engine to create PDFs.',
+	'category' => 'frontend',
+	'author' => 'Christian Zenker',
+	'author_email' => 'http://www.599media.de',
+	'author_company' => '599media, Freiberg',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => 'bottom',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '0.0.0-dev',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => null,
+);
+
+?>
