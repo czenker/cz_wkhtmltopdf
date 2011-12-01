7774bc0f3265ba4afafa68f8d90e29cbf561df06
Christian Zenker <christian.zenker@599media.de>
2011-12-01 11:14:37 +0100

    [~TASK] configured hooks

--- Classes/Controller.php
+++ Classes/Controller.php
@@ -7,34 +7,37 @@
 class tx_CzWkhtmltopdf_Controller {
 
 	/**
+	 * called before any page content is send to the browser
+	 *
+	 * @param array $params
+	 * @param tslib_fe $pObj
 	 * @return void
 	 */
-	public function hookAll(&$params, &$obj) {
-//		$this->processHook($params, $obj);
+	public function hookOutput(&$params, $pObj) {
+		if($pObj->no_cache) {
+			$this->processHook($pObj);
+		}
 	}
 
 	/**
+	 * called before anything is written to the cache
+	 *
+	 * @param tslib_fe $pObj
 	 * @return void
 	 */
-	public function hookOutput(&$params, &$obj) {
-		$this->processHook($params, $obj);
-	}
-
-	/**
-	 * @return void
-	 */
-	public function hookCached(&$params, &$obj) {
-//		$this->processHook($params, $obj);
+	public function hook_indexContent($pObj) {
+		if(!$pObj->no_cache) {
+			$this->processHook($pObj);
+		}
 	}
 
 	/**
 	 * process a hook
 	 *
-	 * @param $params
 	 * @param tslib_fe $pObj
 	 * @return void
 	 */
-	public function processHook(&$params, &$pObj) {
+	public function processHook($pObj) {
 
 		$converter = t3lib_div::makeInstance('tx_CzWkhtmltopdf_Converter');
 		$pdfFile = t3lib_div::makeInstance('tx_CzWkhtmltopdf_TemporaryFile');
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,12 +2,16 @@
 if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
-// hook is called after Caching / pages with COA_/USER_INT objects.
-$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = 'EXT:'.$_EXTKEY.'/Classes/Controller.php:&tx_CzWkhtmltopdf_controller->hookOutput';
 
-// hook is called before Caching / pages on their way in the cache.
-$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = 'EXT:'.$_EXTKEY.'/Classes/Controller.php:&tx_CzWkhtmltopdf_Controller->hookAll';
 
-// Hook for post-processing of page content before being cached:
-$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-cached'][] = 'EXT:'.$_EXTKEY.'/Classes/Controller.php:&tx_CzWkhtmltopdf_Controller->hookCached';
+// hook is called before outputting no matter if content comes from cache or was just generated
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = 'EXT:'.$_EXTKEY.'/Classes/Controller.php:&tx_CzWkhtmltopdf_Controller->hookOutput';
+
+// called before page is written to cache (if it is supposed to)
+/* NOTE: we don't use contentPostProc-cached here as there is some character conversion done
+ * afterwards and this might break the encoding (although UTF8 should not be problematic)
+ */
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['pageIndexing'][] = 'tx_CzWkhtmltopdf_Controller';
+
+
 ?>
