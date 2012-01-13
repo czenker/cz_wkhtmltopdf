b69527c30187890509db103f223aaf24f867deb3
Christian Zenker <christian.zenker@599media.de>
2012-01-13 12:36:58 +0100

    [TASK] added more LICENSE information

--- Classes/Config.php
+++ Classes/Config.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2
+ *
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                   Version 2, December 2004
+ *
+ * Copyright (C) 2012 Christian Zenker <christian.zenker@599media.de>
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ * 0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+
 
 /**
  * A class holding configuration from the extensions configuration in TYPO3_CONF_VARS
